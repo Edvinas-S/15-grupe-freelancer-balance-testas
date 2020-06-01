902c007b365473b58cd8c46e380855f617ddb681
Edvinas <63248412+Edvinas-S@users.noreply.github.com>
2020-06-01 13:52:43 +0300

    Pakeistas html title

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 const menesiai = ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'];
 console.log(menesiai);
+
+const lentele = document.querySelector('.table-content');
+console.log(lentele);
+
+for (let i=0; i < account.length; i++) {
+let HTML = ``;
+HTML = `<div class="table-row">
+                <div class="cell">${account[i].month}</div>
+                <div class="cell">${menesiai[i]}</div>
+                <div class="cell">${account[i].income}Eur</div>
+                <div class="cell">${account[i].expense}Eur</div>
+                <div class="cell">${account[i].income - account[i].expense}Eur</div>
+            </div>`
+lentele.innerHTML += HTML;
+}

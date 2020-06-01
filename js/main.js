0b0fa6113cffc6d8e6ba1f18bf2657911ffc7083
Edvinas <63248412+Edvinas-S@users.noreply.github.com>
2020-06-01 14:06:36 +0300

    Footer dalis

--- js/main.js
+++ js/main.js
@@ -2,16 +2,30 @@ const menesiai = ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Biržel
 console.log(menesiai);
 
 const lentele = document.querySelector('.table-content');
-console.log(lentele);
+
+const footer = document.querySelector('.table-footer');
 
 for (let i=0; i < account.length; i++) {
-let HTML = ``;
-HTML = `<div class="table-row">
+let HTMLrow = ``;
+HTMLrow = `<div class="table-row">
                 <div class="cell">${account[i].month}</div>
                 <div class="cell">${menesiai[i]}</div>
                 <div class="cell">${account[i].income}Eur</div>
                 <div class="cell">${account[i].expense}Eur</div>
                 <div class="cell">${account[i].income - account[i].expense}Eur</div>
             </div>`
-lentele.innerHTML += HTML;
+lentele.innerHTML += HTMLrow;
+}
+
+for (let i=0; i < account[i].length; i++) {
+let HTMLfooter = ``;
+let islaidos = 0;
+let pajamos = 0;
+let bendra = 0;
+HTMLfooter = `<div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell">${pajamos += account[i].income} Eur</div>
+        <div class="cell">${islaidos += account[i].expense} Eur</div>
+        <div class="cell">${bendra = pajamos - islaidos} Eur</div>`
+footer.innerHTML = HTMLfooter;
 }
